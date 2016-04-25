c40e4395055536933667854169156c6b9188b17a
AND <janaipakos@gmail.com>
2016-04-25 12:41:24 -0400

    Initial commit

---
+++ app_client/home/home.controller.js
@@ -0,0 +1,55 @@
+(function () {
+
+  angular
+    .module('loc8rApp')
+    .controller('homeCtrl', homeCtrl);
+
+  homeCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
+  function homeCtrl ($scope, loc8rData, geolocation) {
+    // Nasty IE9 redirect hack (not recommended)
+    if (window.location.pathname !== '/') {
+      window.location.href = '/#' + window.location.pathname;
+    }
+    var vm = this;
+    console.log(window.location);
+    vm.pageHeader = {
+      title: 'Dark Souls NYC',
+      strapline: 'The world of Lordran separated into Manhattan neighborhoods'
+    };
+    vm.sidebar = {
+      content: "Dark Souls NYC can help you find new and familiar locations from Lordran, no Lordvessel necessary."
+    };
+    vm.message = "Checking your location";
+
+    vm.getData = function (position) {
+      var lat = position.coords.latitude,
+          lng = position.coords.longitude;
+      vm.message = "Searching for nearby places";
+      loc8rData.locationByCoords(lat, lng)
+        .success(function(data) {
+          vm.message = data.length > 0 ? "" : "No locations found nearby";
+          vm.data = { locations: data };
+          console.log(vm.data);
+        })
+        .error(function (e) {
+          vm.message = "Sorry, something's gone wrong, please try again later";
+        });
+    };
+
+    vm.showError = function (error) {
+      $scope.$apply(function() {
+        vm.message = error.message;
+      });
+    };
+
+    vm.noGeo = function () {
+      $scope.$apply(function() {
+        vm.message = "Geolocation is not supported by this browser.";
+      });
+    };
+
+    geolocation.getPosition(vm.getData,vm.showError,vm.noGeo);
+
+  }
+
+})();
